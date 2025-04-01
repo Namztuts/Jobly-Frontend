85dcf5f09aa0c9e6e9d48ba101c63699a8ac5e49
Jacob S <me@mydomain.com>
2025-03-31 20:08:54 -0500

    Updated base_url for Vite

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+// const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3001'; //for Vite
+
 // const BASE_URL = 'http://localhost:3001';
 /* API Class
  *
